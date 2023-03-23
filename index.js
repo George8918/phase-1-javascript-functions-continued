cb92ac8763f8a0f87af92db56f2cc0265077930c
George <GZheng8918@gmail.com>
2023-03-22 23:34:24 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun (activity= 'roller-skate') {
+return `This Saturday, I want to ${activity}!`
+}
+
+
+function mondayWork(activityForWork = 'go to the office') {
+ return `This Monday, I will ${activityForWork}.` 
+}
+function wrapAdjective(flair) {
+    if (flair === undefined) {
+      flair = "*";
+    }
+    return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
